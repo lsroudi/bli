dfdafb9317051fe3c0cbb37731281a77589f7edb
Abdessamad el akroudi <lsroudi@gmail.com>
2025-03-09 22:21:31 +0100

    add genesis block

--- blockchain_rs/src/main.rs
+++ blockchain_rs/src/main.rs
@@ -1,6 +1,6 @@
-use serde::{Serialize,Deserialize};
-use sha2::{Sha256,Digest};
 use chrono::Utc;
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Block {
@@ -11,27 +11,50 @@ struct Block {
     hash: String,
 }
 
-impl Block{
-
-    fn calculate_hash(index:u64, timestamp:i64, data: &str, previous_hash: &str) -> String{
-        let input = format!("{},{},{},{}" ,index, timestamp, data, previous_hash);
+impl Block {
+    fn calculate_hash(index: u64, timestamp: i64, data: &str, previous_hash: &str) -> String {
+        let input = format!("{},{},{},{}", index, timestamp, data, previous_hash);
         let mut hasher = Sha256::new();
         hasher.update(input);
         hex::encode(hasher.finalize())
     }
 
-    fn genesis() ->Self {
-        let index = 0; 
-        let timestamp = Utc::now().timestamp(); 
+    fn genesis() -> Self {
+        let index = 0;
+        let timestamp = Utc::now().timestamp();
         let data = String::from("the is the first block");
-        let previous_hash= String::from("0");
+        let previous_hash = String::from("0");
         let hash = Block::calculate_hash(index, timestamp, &data, &previous_hash);
 
-        Block {index, timestamp, data, previous_hash, hash}
+        Block {
+            index,
+            timestamp,
+            data,
+            previous_hash,
+            hash,
+        }
     }
 }
 
-fn main(){
-    println!(" this is the first line in our journey of 
-               a BlockChain from scratch");
+#[derive(Debug)]
+struct BlockChain {
+    chain: Vec<Block>,
+}
+
+impl BlockChain {
+    fn new() -> Self {
+        BlockChain {
+            chain: vec![Block::genesis()],
+        }
+    }
+}
+
+fn main() {
+    println!(
+        " this is the first line in our journey of 
+               a BlockChain from scratch"
+    );
+
+    let blockchain = BlockChain::new();
+    println!("the genesis block is {:#?}", blockchain);
 }
