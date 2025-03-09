f6c9036da8c5cfb1ca3b6fb148f135a694cdb9cf
Abdessamad el akroudi <lsroudi@gmail.com>
2025-03-09 17:35:55 +0100

    add calculate hash function

--- blockchain_rs/src/main.rs
+++ blockchain_rs/src/main.rs
@@ -11,6 +11,16 @@ struct Block {
     hash: String,
 }
 
+impl Block{
+
+    fn calculate_hash(index:u64, timestamp:i64, data: &str, previous_hash: &str) -> String{
+        let input = format!("{},{},{},{}" ,index, timestamp, data, previous_hash);
+        let mut hasher = Sha256::new();
+        hasher.update(input);
+        hex::encode(hasher.finalize())
+    }
+}
+
 fn main(){
     println!(" this is the first line in our journey of 
                a BlockChain from scratch");
